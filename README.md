0a378b3ca7fcf5223017d9145f1a1a0caedc6e8c
Yoshimasa Katakura <yoshimasa.katakura@outlook.com>
2021-06-30 10:11:48 +0900

    Initial Commit

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# aztools
+# README
+
+## azure-resource-get.sh
+
+This program will run the Azure CLI "az resource show" command for all resources in the specified resource group.
+
+The output json file will be created and saved in a directory for each resource provider under the current directory.
+
+The version of Azure CLI is confirmed to be 2.25.0, but even if it is a little older, it will not be a problem.
+
+usage:
+
+```bash
+$ ./azure-resource-get.sh <Resource Group Name...>
+```
+
+example(Targeting all resource groups):
+
+```bash
+$ mkdir 20210630
+$ cd 20210640
+$ ../azure-resource-get.sh $(az group list -o tsv --query '[].name')
+```
---
+++ azure-resource-get.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#
+# Azure resource dump tool
+# Created by Y.Katakura
+#
+# usage: ./azure-resource-get.sh <Resource Group Name...>
+#
+# example:
+# $ mkdir 20210630
+# $ cd 20210640
+# $ ../azure-resource-get.sh $(az group list -o tsv --query '[].name')
+
+for arg in "$@"; do
+    echo "#### ${arg} ####"
+    TARGET_RG=${arg}
+
+    az resource list -g ${TARGET_RG} --query "[].{Name:name,Type:type,ID:id}" -o tsv | sed 's/\t/,/g' | while read line; do
+        IFS=','
+        set -- $line
+        echo $1" ("$2")"
+        OUTFILE="${TARGET_RG}/$2/$1.json"
+        mkdir -p $(dirname ${OUTFILE})
+
+        az resource show -g ${TARGET_RG} --ids "$3" -o json >${OUTFILE}
+    done
+done
